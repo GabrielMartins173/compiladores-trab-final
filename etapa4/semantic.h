d516e86723b0ebc741cb4768753fdd097da084e9
Gabriel Martins <gabrielmartins173@gmail.com>
2022-03-28 21:47:09 -0300

    updating semantic.h

--- etapa4/semantic.h
+++ etapa4/semantic.h
@@ -8,7 +8,14 @@
 #define FALSE 0
 
 int getNumberOfSemanticErrors();
-
+int isValidOperand(AST *node);
+int isValidDeclaration(AST *node);
+void verifyUndefined();
+void verifyDeclarations(AST *node);
+void setDataType(AST *node);
+void verifyOperands(AST *node);
+void printErrorDuringDeclaration(AST *node, char* errorMessage);
+void printErrorDuringOperation(AST *node, char* operand, char* operation);
 
 
 #endif
